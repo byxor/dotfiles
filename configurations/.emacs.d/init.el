909bf600b928486a7708d2599ec9c5653c3a02e0
byxor <brandnewbyxor@gmail.com>
2017-07-27 21:33:17 +0100

    Add elpa support

--- configurations/.emacs.d/init.el
+++ configurations/.emacs.d/init.el
@@ -1,4 +1,9 @@
-(load-file "./arista-loader.el")
+(defun relatively-import (file-name)
+  (load-file
+   (expand-file-name file-name (file-name-directory load-file-name))))
+
+(relatively-import "melpa-loader.el")
+(relatively-import "arista-loader.el")
 
 (column-number-mode 1)
 (show-paren-mode 1)
@@ -6,3 +11,6 @@
 (global-linum-mode 1)
 (setq inhibit-startup-screen t)
 (setq initial-scratch-message nil)
+
+(custom-set-variables
+ '(markdown-command "/usr/bin/pandoc"))
