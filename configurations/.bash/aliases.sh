cdd686f28d767869366f6012a23fef27ca024e03
byxor <brandnewbyxor@gmail.com>
2017-07-27 11:56:48 +0100

    Add passflip to aliases

--- configurations/.bash/aliases.sh
+++ configurations/.bash/aliases.sh
@@ -5,3 +5,5 @@ alias sus="systemctl suspend"
 alias venv="source venv/bin/activate"
 
 alias gitlog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+alias pf="passflip"
