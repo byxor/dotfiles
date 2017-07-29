653bf026f6c3cdcd39af8023dcd11146662b9f43
byxor <brandnewbyxor@gmail.com>
2017-07-29 20:48:17 +0100

    Add alias

--- configurations/.bash/aliases.sh
+++ configurations/.bash/aliases.sh
@@ -7,3 +7,5 @@ alias venv="source venv/bin/activate"
 alias gitlog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 alias pf="passflip"
+
+alias cfg="cd ~/projects/configurations"
