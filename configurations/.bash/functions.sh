fb3e7544957779261483dfcf2acb4aac29f70bf2
byxor <brandnewbyxor@gmail.com>
2017-08-08 07:42:52 -0700

    Add strip and word functionality to bash

--- configurations/.bash/functions.sh
+++ configurations/.bash/functions.sh
@@ -4,8 +4,8 @@ function pydir {
 }
 
 function hardreset {
-    NEWLINES=300
-    for _ in $(seq $NEWLINES); do
+    local new_lines=300
+    for _ in $(seq $new_lines); do
         echo
     done
     reset
@@ -13,4 +13,27 @@ function hardreset {
 
 function rm {
     echo "This command is very dangerous. Please use 'trash' instead. I know it's annoying, but you'll thank me later. I promise."
+}
+
+function strip {
+    local input=$1
+
+    local output=$input
+
+    # remove leading whitespace characters
+    local output="${output#"${output%%[![:space:]]*}"}"
+
+    # remove trailing whitespace characters
+    local output="${output%"${output##*[![:space:]]}"}"
+
+    echo $output
+}
+
+
+function word {
+    local input=$(cat)
+    local words=($input)
+    local word=${words[$1]}
+    local stripped_word=$(strip $word)
+    echo $stripped_word
 }
