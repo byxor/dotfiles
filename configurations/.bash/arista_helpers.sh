234ea643e62ff5068f3777e116524d15d00dd568
byxor <brandnewbyxor@gmail.com>
2017-08-10 05:51:04 -0700

    Add container-last function to bash

--- configurations/.bash/arista_helpers.sh
+++ configurations/.bash/arista_helpers.sh
@@ -39,8 +39,16 @@ function dut-release {
 }
 
 function dut-release-all {
-    local grabbed_duts=$(Art grabbed)
-    echo "$grabbed_duts" | xargs dut-release
+    if [ $(dut-number) -gt 0 ]
+    then
+        local grabbed_duts=$(Art grabbed)
+        for grabbed_dut in ${grabbed_duts//\\n/ }
+        do
+            $(dut-release $grabbed_dut)
+        done
+    else
+        echo "No duts are grabbed."
+    fi
 }
 
 function dut-ssh {
@@ -99,6 +107,41 @@ function containers {
     a4c containers
 }
 
+LAST_CONTAINER_FILE_NAME=$HOME/.container.last
+
+function __container-last-set {
+    if ! [ -z "$1" ]
+    then
+        local container=$1
+        echo $container > $LAST_CONTAINER_FILE_NAME
+    else
+        echo "Please provide a value for the container."
+    fi
+}
+
+function __container-last-get {
+    if [ -f $LAST_CONTAINER_FILE_NAME ]
+    then
+        cat $LAST_CONTAINER_FILE_NAME
+    fi
+}
+
 function container {
-    a4c shell $1
+    if ! [ -z "$1" ]
+    then
+       __container-last-set $1
+       a4c shell $1
+    else
+        echo "Please provide a value for the container."
+    fi
+}
+
+function container-last {
+    local last_container=$(__container-last-get)
+    if [ -n "$last_container" ]
+    then
+        a4c shell $last_container
+    else
+        echo "There is no known 'last container'."
+    fi
 }
