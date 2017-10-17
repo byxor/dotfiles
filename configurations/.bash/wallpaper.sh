41370692755eb30199a68ae69f8162dcea35eadc
byxor <brandnewbyxor@gmail.com>
2017-10-17 21:36:31 +0100

    Improve wallpapers

--- configurations/.bash/wallpaper.sh
+++ configurations/.bash/wallpaper.sh
@@ -1,1 +1,9 @@
-feh --bg-fill ~/Pictures/Wallpapers/CURRENT
+local WALLPAPER_DIRECTORY="$HOME/Pictures/Wallpapers"
+
+function wallpaper-random {
+    local wallpaper=$(ls -1 $WALLPAPER_DIRECTORY | shuf | tail -n 1)
+    local final_path=$WALLPAPER_DIRECTORY/$wallpaper
+    feh --bg-fill $final_path
+}
+
+wallpaper-random
