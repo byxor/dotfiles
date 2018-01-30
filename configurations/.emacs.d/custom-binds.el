6431628d596e33c2b92074b977a96b266f49ca78
byxor <brandnewbyxor@gmail.com>
2018-01-30 07:02:59 -0800

    Improve emacs

--- configurations/.emacs.d/custom-binds.el
+++ configurations/.emacs.d/custom-binds.el
@@ -1,3 +1,14 @@
-(global-set-key (kbd "C-x C-a") 'ace-window)
-(global-set-key (kbd "C-z") 'zoom-window-zoom)
-(global-set-key (kbd "C-c C-t") 'delete-trailing-whitespace)
+(setq PREFIX "C-z")
+
+(defun custom-shortcut (shortcut)
+  (kbd (format "%s %s" PREFIX shortcut)))
+
+(global-set-key (kbd PREFIX) nil)
+  
+(global-set-key (custom-shortcut "C-z") 'zoom-window-zoom)
+(global-set-key (custom-shortcut "C-t") 'delete-trailing-whitespace)
+(global-set-key (custom-shortcut "C-a") 'ace-window)
+(global-set-key (custom-shortcut "C-l") 'linum-mode)
+(global-set-key (custom-shortcut "C-s") 'sr-speedbar-toggle)
+(global-set-key (custom-shortcut "C-c") 'comment-region)
+(global-set-key (custom-shortcut "M-c") 'uncomment-region)
--- configurations/.emacs.d/default-modes.el
+++ configurations/.emacs.d/default-modes.el
@@ -2,8 +2,6 @@
 (show-paren-mode 1)
 (ido-mode 1)
 (ac-config-default)
-(global-linum-mode 1)
-(neotree)
 
 (add-to-list 'load-path "/directory/containing/neotree/")
 (require 'neotree)
