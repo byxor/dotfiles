f8c98bf8e92eb27ae9de0146c51d499e6ad0da8e
byxor <brandnewbyxor@gmail.com>
2018-04-04 07:48:07 -0700

    Add more emacs binds

--- configurations/.emacs.d/custom-binds.el
+++ configurations/.emacs.d/custom-binds.el
@@ -6,7 +6,7 @@
 (global-set-key (kbd PREFIX) nil)
   
 (global-set-key (custom-shortcut "C-z") 'zoom-window-zoom)
-(global-set-key (custom-shortcut "C-t") 'delete-trailing-whitespace)
+(global-set-key (custom-shortcut "C-T") 'delete-trailing-whitespace)
 (global-set-key (custom-shortcut "C-a") 'ace-window)
 (global-set-key (custom-shortcut "C-l") 'linum-mode)
 (global-set-key (custom-shortcut "C-s") 'sr-speedbar-toggle)
@@ -14,3 +14,5 @@
 (global-set-key (custom-shortcut "M-c") 'uncomment-region)
 (global-set-key (custom-shortcut "C-n") 'neotree)
 (global-set-key (custom-shortcut "C-e") 'electric-indent-mode)
+(global-set-key (custom-shortcut "C-t") 'customize-themes)
+(global-set-key (custom-shortcut "C-p") 'list-packages)
