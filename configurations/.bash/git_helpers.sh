48d6c973456a1fe6a3ee22d2dc434cad4e426f01
byxor <brandnewbyxor@gmail.com>
2018-04-26 13:53:23 +0100

    Add lazygit

--- configurations/.bash/git_helpers.sh
+++ configurations/.bash/git_helpers.sh
@@ -1,3 +1,19 @@
 alias git-log="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 git config --global alias.add-commit '!git add -A && git commit'
+
+function lazygit() {
+  git add .
+  if git commit -a -m "$1"; then
+    read -r -p "Are you sure you want to push these changes? [y/N]} " response
+    case "$response" in
+      [yY][eE][sS]|[yY])
+        git push
+        ;;
+      *)
+        git reset HEAD~1 --soft
+        echo "Reverted changes."
+        ;;
+    esac
+  fi
+}
