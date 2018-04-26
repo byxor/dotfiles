c69923c9a67ca27fa731aaa9dbf4e253c0ab5d80
byxor <brandnewbyxor@gmail.com>
2018-04-26 13:19:10 +0100

    Add git alias

--- configurations/.bash/git_helpers.sh
+++ configurations/.bash/git_helpers.sh
@@ -1,1 +1,3 @@
 alias git-log="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+git config --global alias.add-commit '!git add -A && git commit'
