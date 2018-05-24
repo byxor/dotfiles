4c2f63d89e316f39fe318d9c77554483af0e160f
byxor <brandnewbyxor@gmail.com>
2018-05-24 20:30:21 +0100

    Add cmake to path

--- configurations/.bash/exports.sh
+++ configurations/.bash/exports.sh
@@ -6,4 +6,5 @@ export GTEST_DIR=$HOME/installers/googletest-release-1.8.0/googletest
 
 export PATH=$PATH:$HOME/software/ripgrep-0.5.2-i686-unknown-linux-musl
 export PATH=$PATH:$HOME/software/gradle-4.3.1/bin
-export PATH=~/.npm-global/bin:$PATH
+export PATH=$PATH:$HOME/software/cmake-3.11.2-Linux-x86_64/bin
+export PATH=$PATH:$HOME/.npm-global/bin
