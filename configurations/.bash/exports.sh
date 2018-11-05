63db987cb0dd9da50f509d57e4e2e1e87fab4501
byxor <brandnewbyxor@gmail.com>
2018-11-05 14:56:05 +0000

    Add javacc to path

--- configurations/.bash/exports.sh
+++ configurations/.bash/exports.sh
@@ -5,6 +5,7 @@ export PATH=$PATH:$HOME/installers/leiningen
 export GTEST_DIR=$HOME/installers/googletest-release-1.8.0/googletest
 
 export PATH=$PATH:$HOME/software/ripgrep-0.5.2-i686-unknown-linux-musl
+export PATH=$PATH:$HOME/software/javacc-6.0/bin/lib
 export PATH=$PATH:$HOME/software/gradle-4.3.1/bin
 export PATH=$PATH:$HOME/software/cmake-3.11.2-Linux-x86_64/bin
 export PATH=$PATH:$HOME/.npm-global/bin
