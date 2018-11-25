eba93a3bd35bf7240ae817e427d95805de123c0a
byxor <brandnewbyxor@gmail.com>
2018-11-25 06:59:25 +0000

    Add gba tools to path

--- configurations/.bash/exports.sh
+++ configurations/.bash/exports.sh
@@ -8,6 +8,9 @@ export PATH=$PATH:$HOME/software/ripgrep-0.5.2-i686-unknown-linux-musl
 export PATH=$PATH:$HOME/software/javacc-6.0/bin/lib
 export PATH=$PATH:$HOME/software/gradle-4.3.1/bin
 export PATH=$PATH:$HOME/software/cmake-3.11.2-Linux-x86_64/bin
+export PATH=$PATH:$HOME/software/cmake-3.11.2-Linux-x86_64/bin
+export PATH=$PATH:$HOME/software/devkitadv/bin
+
 export PATH=$PATH:$HOME/.npm-global/bin
 export PATH=$PATH:/usr/local/go/bin
 
