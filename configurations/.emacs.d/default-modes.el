71d601680006a81c869c0916bf89d8d447cc2f60
byxor <brandnewbyxor@gmail.com>
2019-01-02 18:57:52 +0000

    Update emacs config

--- configurations/.emacs.d/default-modes.el
+++ configurations/.emacs.d/default-modes.el
@@ -2,6 +2,9 @@
 (show-paren-mode 1)
 (electric-indent-mode 0)
 (ido-mode 1)
+(tool-bar-mode -1)
+(menu-bar-mode -1) 
+
 (ac-config-default)
 (setq speedbar-initial-expansion-list-name "quick buffers")
 (add-to-list 'load-path "/directory/containing/neotree/")
