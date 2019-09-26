76ab79a6eb547f9ef2eaae198eabd593be7f7af7
byxor <brandnewbyxor@gmail.com>
2019-09-26 12:05:25 +0100

    Add basic mac support

--- configurations/.bash/exports.sh
+++ configurations/.bash/exports.sh
@@ -1,26 +1,29 @@
-# Editors
-export EDITOR=emacs
+if [ "$(uname)" == "Darwin" ]; then
+  echo "On Mac; not exporting environment variables."
+else
+  # Editors
+  export EDITOR=emacs
 
-# PATH
-export PATH=$PATH:$HOME/software/bin
-export PATH=$PATH:/snap/bin
+  # PATH
+  export PATH=$PATH:$HOME/software/bin
+  export PATH=$PATH:/snap/bin
 
-# Golang
-export GOPATH=$HOME/projects/go
-export PATH=$PATH:$GOPATH/bin
-export PATH=$PATH:/usr/local/go/bin
+  # Golang
+  export GOPATH=$HOME/projects/go
+  export PATH=$PATH:$GOPATH/bin
+  export PATH=$PATH:/usr/local/go/bin
 
-# Android
-export ANDROID_HOME=$HOME/Android/Sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/tools/bin
-export PATH=$PATH:$ANDROID_HOME/platform-tools
+  # Android
+  export ANDROID_HOME=$HOME/Android/Sdk
+  export PATH=$PATH:$ANDROID_HOME/emulator
+  export PATH=$PATH:$ANDROID_HOME/tools
+  export PATH=$PATH:$ANDROID_HOME/tools/bin
+  export PATH=$PATH:$ANDROID_HOME/platform-tools
 
-# Java
-export JDK_18="/usr/lib/jvm/java-8-oracle"
-export JAVA_HOME=$JDK_18
-export PATH=$PATH:$JDK_18/bin
-export JDK_16=$JDK_18
-export JDK_17=$JDK_18
-
+  # Java
+  export JDK_18="/usr/lib/jvm/java-8-oracle"
+  export JAVA_HOME=$JDK_18
+  export PATH=$PATH:$JDK_18/bin
+  export JDK_16=$JDK_18
+  export JDK_17=$JDK_1
+fi
