7c1a274f59a803cdd7acf344c5f3833658e39307
bibbotson <bibbotson@atlassian.com>
2020-03-09 15:58:31 +0000

    Temporarily disable git branch in prompt

--- configurations/.bash/git/shell_prompt.sh
+++ configurations/.bash/git/shell_prompt.sh
@@ -2,5 +2,5 @@ function get_current_git_branch {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 }
 
-export PS1="\u@\h \[\033[32m\]\w\[\033[33m\] ($(get_current_git_branch))\[\033[00m\] $ "
+export PS1="\u@\h \[\033[32m\]\w\[\033[00m\] $ "
 
